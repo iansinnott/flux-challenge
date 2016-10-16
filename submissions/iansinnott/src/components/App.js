1305d768c1aa0d29d03dc6b6e69e8bc3f71d2257
Ian Sinnott <ian@iansinnott.com>
2016-10-15 21:38:36 -0700

    Add planet monitor component

--- submissions/iansinnott/src/components/App.js
+++ submissions/iansinnott/src/components/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 import './styles.css';
 import SithList from './SithList.js';
+import PlanetMonitor from './PlanetMonitor.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app-container'>
         <div className='css-root'>
-          <h1 className='css-planet-monitor'>Obi-Wan currently on Tatooine</h1>
+          <PlanetMonitor />
 
           <section className='css-scrollable-list'>
             <SithList />
---
+++ submissions/iansinnott/src/components/PlanetMonitor.js
@@ -0,0 +1,18 @@
+/* @flow */
+import React from 'react';
+import { connect } from 'react-redux';
+
+type PlanetMonitorProps = {
+  planetName: string,
+};
+const PlanetMonitor = ({ planetName }: PlanetMonitorProps) => (
+  <h1 className='css-planet-monitor'>
+    Obi-Wan currently on {planetName}
+  </h1>
+);
+
+const mapStateToProps = (state) => ({
+  planetName: state.getIn(['planetMonitor', 'planetName']),
+});
+
+export default connect()(PlanetMonitor);
